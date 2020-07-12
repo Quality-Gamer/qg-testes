b9a3191f6e52171caca8f61acefc3261c9528851
Agualuza <iagoagualuza@id.uff.br>
2020-07-12 12:22:10 -0300

    Revert "Update main.py"
    This reverts commit a3377cd7fc9cdfe66af4a76444f628cc3069984f.

--- main.py
+++ main.py
@@ -13,46 +13,46 @@ def main():
 
 @app.route("/api/tests",methods=['POST'])
 def tests():
-    email = str(request.form.get('email'))
-    password = str(request.form.get('password'))
+    email = str(request.form('email'))
+    password = str(request.form('password'))
 
     return jsonify(testsEndpoint(email,password))
 
 @app.route("/api/done",methods=['POST'])
 def done():
-    email = str(request.form.get('email'))
-    password = str(request.form.get('password'))
+    email = str(request.form('email'))
+    password = str(request.form('password'))
 
     return jsonify(testsDoneEndpoint(email,password))
 
 @app.route("/api/questions",methods=['POST'])
 def questions():
-    email = str(request.form.get('email'))
-    password = str(request.form.get('password'))
-    match_id = str(request.form.get('match_id'))
-    order = str(request.form.get('order'))
+    email = str(request.form('email'))
+    password = str(request.form('password'))
+    match_id = str(request.form('match_id'))
+    order = str(request.form('order'))
 
     return jsonify(questionsEndpoint(email,password,match_id,order))
 
 @app.route("/api/save",methods=['POST'])
 def save():
-    email = str(request.form.get('email'))
-    password = str(request.form.get('password'))
-    match_id = str(request.form.get('match_id'))
-    order = str(request.form.get('order'))
-    user_id = int(request.form.get('user_id'))
-    option = str(request.form.get('option'))
+    email = str(request.form('email'))
+    password = str(request.form('password'))
+    match_id = str(request.form('match_id'))
+    order = str(request.form('order'))
+    user_id = int(request.form('user_id'))
+    option = str(request.form('option'))
 
     return jsonify(saveAnswerEndpoint(email,password,match_id,order,user_id,option))
 
 @app.route("/api/end",methods=['POST'])
 def end():
-    email = str(request.form.get('email'))
-    password = str(request.form.get('password'))
-    match_id = str(request.form.get('match_id'))
-    user_id = int(request.form.get('user_id'))
-    test_id = int(request.form.get('test_id'))
-    win = int(request.form.get('win'))
+    email = str(request.form('email'))
+    password = str(request.form('password'))
+    match_id = str(request.form('match_id'))
+    user_id = int(request.form('user_id'))
+    test_id = int(request.form('test_id'))
+    win = int(request.form('win'))
 
     return jsonify(endTestEndpoint(email,password,match_id,user_id,test_id,win))
 
